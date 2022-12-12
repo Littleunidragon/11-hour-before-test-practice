035445fb5ce80ff3ee167dbfcd85980d5bc89a30
Littleunidragon <81423410+Littleunidragon@users.noreply.github.com>
2022-12-12 10:35:54 +0200

    Add files via upload

--- main.go
+++ main.go
@@ -1,33 +1,46 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"io"
-	"os"
 )
 
-type node struct {
-	val      int
-	children []int
+func IsHeap(s []int) bool {
+	if len(s) == 0 {
+		return true
+	}
+	for i := 0; i < (len(s)/2)-1; i++ {
+		if (2*i)+1 >= s[i] || (2*i)-1 >= len(s)-1 {
+			if s[(i-1)/2] > s[i] {
+				return false
+			}
+		}
+	}
+	return true
 }
 
-func ans(r io.Reader) {
-	var n, m int
-	fmt.Fscan(r, &n, &m)
-	cats := make([]int, n)
-	for i := range cats {
-		fmt.Fscan(r, &cats[i])
+func swords(r io.Reader) []int {
+	var age []int
+	var n, k int
+	fmt.Scan(&n, &k)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(r, &age[i])
 	}
-	coord := make(map[int]int)
-	for i := 0; i < n-1; i++ {
-		var x, y int
-		fmt.Fscan(r, &x, &y)
-		coord[x] = append(coord[x], x)
-	}
+	fmt.Println(n, k, age)
+	return []int{0}
 }
 
 func main() {
-	r := bufio.NewReader(os.Stdin)
-	ans(r)
+	var s, a, d []int
+	s = append(s, 1, 5, 3, 8, 7, 4, 9, 13, 15)
+	a = append(a, 78, 5, 3, 8, 7, 4, 9, 13, 15)
+	d = append(d, 1, 3, 2, 5, 8, 9, 6, 6, 7, 9, 3)
+	fmt.Println(s)
+	fmt.Println(IsHeap(s))
+	fmt.Println(a)
+	fmt.Println(IsHeap(a))
+	fmt.Println(d)
+	fmt.Println(IsHeap(d))
+	//r := bufio.NewReader(os.Stdout)
+	//swords(r)
 }
